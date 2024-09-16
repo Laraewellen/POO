57c1dff2384cbbf938d1581ad0bbf60d914e6940
LaraeEwellen <Laraewellen@gmail.com>
2024-09-16 10:43:18 -0300

    update

---
+++ sala_atvd/cos.java
@@ -0,0 +1,25 @@
+public class cos
+{
+    public static void main(String[] args) {
+        int num, fatorial;
+        double x=90, cos=o, j=0;
+
+        num=4;
+
+        x= x*3,14/180.0;
+
+        for(int i=0; i<10; i+=2){
+            num=i;
+            fatorial=1;
+            while(num>=1){
+            fatorial = fatorial*num;
+            num--;
+            }
+        }
+        cos = cos + Math.pow(-1,j)*Math.pow(xRad,i)/fatorial;
+    j++;
+    }
+
+    System.out.printf("0 cos(%.1f) = %.3", x, cos);
+
+}
