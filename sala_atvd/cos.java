37ba8d255d8f324dcc15fa0ba99623438078f058
Lara Ewellen <laraewellen@gmail.com>
2024-09-16 17:51:38 -0300

    update

--- sala_atvd/cos.java
+++ sala_atvd/cos.java
@@ -1,25 +1,34 @@
 public class cos
 {
-    public static void main(String[] args) {
-        int num, fatorial;
-        double x=90, cos=o, j=0;
+	public static void main(String[] args)
+	{
+		int num, fatorial;
+		double x=90, cos=0, j=0;
+		num=4;
 
-        num=4;
+		/*for(int count=num; count>=1; count--)
+		{
+			fatorial = fatorial*count;
+			
+		}*/
 
-        x= x*3,14/180.0;
+		double xRad = x*3.14/180.0;
+		for(int i=0; i<10;i+=2)
+		{
+			num=i;
+			fatorial=1;
+			while(num>=1)
+			{
+				fatorial = fatorial*num;
+				num--;
+			}
 
-        for(int i=0; i<10; i+=2){
-            num=i;
-            fatorial=1;
-            while(num>=1){
-            fatorial = fatorial*num;
-            num--;
-            }
-        }
-        cos = cos + Math.pow(-1,j)*Math.pow(xRad,i)/fatorial;
-    j++;
-    }
+			cos = cos + Math.pow(-1,j)*Math.pow(xRad,i)/fatorial;
+			j++;
+		}
+		
 
-    System.out.printf("0 cos(%.1f) = %.3", x, cos);
-
-}
+		System.out.printf("O cos(%.1f) = %.3f", x, cos);
+				
+	}
+} 
