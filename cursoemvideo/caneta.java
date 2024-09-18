0ef788f50c9b8c79ab62f3ae06925b90d88bb960
Lara Ewellen <134017004+Laraewellen@users.noreply.github.com>
2024-09-18 09:48:21 -0300

    Update Caneta.java

--- cursoemvideo/caneta.java
+++ cursoemvideo/caneta.java
@@ -1,10 +1,37 @@
-public class caneta {
-    public static void main(String[] args){
-      caneta c1 = new caneta(); //criando objeto instaciado
-      c1.cor = "vermelha";
-      c1.ponta = 0.5f ; //atributo
-      c1.destampar(); 
-      c1.status(); //metodo 
-      c1.rabiscar();
+public class Caneta {
+    // Atributos da classe Caneta
+    String cor;
+    float ponta;
+    boolean tampada;
+
+    // Método para destampar a caneta
+    void destampar() {
+        this.tampada = false;
+    }
+
+    // Método para mostrar o status da caneta
+    void status() {
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+
+    // Método para rabiscar
+    void rabiscar() {
+        if (this.tampada) {
+            System.out.println("Erro: Não posso rabiscar, estou tampada!");
+        } else {
+            System.out.println("Estou rabiscando!");
+        }
+    }
+
+    // Método principal
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta(); // Criando objeto instanciado
+        c1.cor = "vermelha";
+        c1.ponta = 0.5f; // Atributo
+        c1.destampar();
+        c1.status(); // Método
+        c1.rabiscar();
     }
 }
