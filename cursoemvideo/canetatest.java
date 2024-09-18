73ce7f0f660271de9166db10a26fdd474374a4ea
Lara Ewellen <134017004+Laraewellen@users.noreply.github.com>
2024-09-18 09:54:06 -0300

    Update canetatest.java

--- cursoemvideo/canetatest.java
+++ cursoemvideo/canetatest.java
@@ -1,39 +1,57 @@
-package caneta;
-public class canetatest {
+package Caneta;
+
+public class Canetatest {
     String modelo;
     String cor;
     float ponta;
     int carga;
     boolean tampada;
-    
-  void status() {
-      System.out.println("Modelo: " + this.modelo);
-      System.out.println("caneta é: " + this.cor);
-      System.out.println("A ponta é: " + this.ponta);
-      System.out.println("A carga é" + this.carga);
-      System.out.println("esta tampada? " + this.tampada);
+
+    // Método para exibir o status da caneta
+    void status() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("A caneta é: " + this.cor);
+        System.out.println("A ponta é: " + this.ponta);
+        System.out.println("A carga é: " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
     }
 
-    void rabiscar(){
-      if (tampada == true) {
-        System.out.printf("erro");
-      } else {
-        System.out.printf("rasbicar");
-      }
-
-      }
-
-    void tampar(){
-
-      this.tampada = true;
-
+    // Método para rabiscar
+    void rabiscar() {
+        if (this.tampada) {
+            System.out.println("Erro: Não posso rabiscar, estou tampada!");
+        } else {
+            System.out.println("Estou rabiscando!");
+        }
     }
 
-    void destampar(){
-
-      this.tampada = false;
-
+    // Método para tampar a caneta
+    void tampar() {
+        this.tampada = true;
     }
 
-  }
+    // Método para destampar a caneta
+    void destampar() {
+        this.tampada = false;
+    }
 
+    // Método principal para testar a classe
+    public static void main(String[] args) {
+        // Criando um objeto da classe Canetatest
+        Canetatest caneta = new Canetatest();
+
+        // Atribuindo valores aos atributos
+        caneta.modelo = "BIC";
+        caneta.cor = "Azul";
+        caneta.ponta = 0.7f;
+        caneta.carga = 100;
+
+        // Exibindo status e testando o comportamento
+        caneta.status();
+        caneta.rabiscar(); // A caneta está tampada, não poderá rabiscar
+
+        // Destampando a caneta e tentando rabiscar novamente
+        caneta.destampar();
+        caneta.rabiscar(); // Agora a caneta poderá rabiscar
+    }
+}
